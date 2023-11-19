403cbac2bfd37455cf0aa4c0d085fbc6d75b8cbb
Danny123-coder-spec <99043002+Danny123-coder-spec@users.noreply.github.com>
2023-11-19 23:55:50 +0000

    Add files via upload

---
+++ middleware.ts
@@ -0,0 +1,7 @@
+export {default}from 'next-auth/middleware'
+ 
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|auth/login).*)'],
+}
